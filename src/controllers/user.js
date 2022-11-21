d6c02d4a9391fd0feff834397ea6434e60269fe2
supertree-kwangmon <kwangmon@supertree.co>
2022-11-21 16:04:42 +0900

    feat: add join api

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,11 +1,31 @@
 import User from "../models/user";
 
-export const JoinUser = (req, res) => {
+export const JoinUser = async (req, res) => {
   try {
+    const { email, password, name, age } = req.body;
+
+    console.log(email, password, name, age);
+
+    const data = new User({
+      email,
+      password,
+      salt: "123",
+      name,
+      age,
+    });
+
+    await data.save(); // User Schema Insert하는 과정
+
+    //RDB
+    //SELECT => 조회
+    //INSERT => 삽입
+    //UPDATE => 수정
+    //DELETE => 삭제
+
     res.send({
       success: true,
       message: null,
-      data: "join user",
+      data: req.body,
     });
   } catch (e) {
     res.send({
