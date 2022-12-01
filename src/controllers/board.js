950f6382c2cb872f4c398ef291d4fb7f287e782b
supertree-kwangmon <kwangmon@supertree.co>
2022-12-01 16:19:40 +0900

    feat: add detail api

--- src/controllers/board.js
+++ src/controllers/board.js
@@ -50,3 +50,28 @@ export const getBoardList = async (req, res) => {
     });
   }
 };
+
+export const getBoard = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { user } = res.locals;
+    const data = await Board.findById({ _id: id });
+    const isMyBoard = user._id.toString() === data.userId.toString();
+    console.log("isMyBoard >", isMyBoard);
+
+    res.send({
+      success: true,
+      message: null,
+      data: {
+        ...data._doc,
+        isMyBoard,
+      },
+    });
+  } catch (e) {
+    res.send({
+      success: false,
+      message: e.message,
+      data: null,
+    });
+  }
+};
